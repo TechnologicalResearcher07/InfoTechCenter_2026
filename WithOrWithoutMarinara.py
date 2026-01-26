1b335378a3d4222032f40ea09b44832f1e359c38
Kiera McKimmy <investedcomputerexplorer07@gmail.com>
2026-01-26 18:48:41 +0000

    Weather Function Calls Current Weather

--- WithOrWithoutMarinara.py
+++ WithOrWithoutMarinara.py
@@ -1,1 +1,17 @@
 #Weather Branch
+
+import random
+
+def weather ():
+    conditions = [
+        "Sunny",
+        "Cloudy",
+        "Stormy",
+        "Snowy",
+        "Rainy",
+        "Windy",
+        "Foggy"
+    ]
+    return random.choice(conditions)
+
+print("Today the weather is", weather()+".")
